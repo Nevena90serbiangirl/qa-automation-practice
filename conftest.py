974c8252e0330eae407668db72efc83d1d17a7dd
Nevena90serbiangirl <nevena.suknovic15@gmail.com>
2026-09-20 11:18:44 +0200

    Add requirements and GitHub Actions CI workflow

--- conftest.py
+++ conftest.py
@@ -1,11 +1,20 @@
-import pytest
+import os
 
+import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture
 def driver():
-    browser = webdriver.Chrome()
+    options = Options()
+
+    if os.getenv("CI") == "true":
+        options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+
+    browser = webdriver.Chrome(options=options)
     browser.maximize_window()
 
     yield browser
