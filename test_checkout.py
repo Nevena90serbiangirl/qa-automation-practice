3d0abfef19811ba9f57fa733025a02e0a0db6446
Nevena90serbiangirl <nevena.suknovic15@gmail.com>
2026-09-26 13:50:12 +0200

    Mark checkout test as smoke

--- test_checkout.py
+++ test_checkout.py
@@ -1,9 +1,11 @@
+import pytest
+
 from pages.cart_page import CartPage
 from pages.checkout_page import CheckoutPage
 from pages.inventory_page import InventoryPage
 from pages.login_page import LoginPage
 
-
+@pytest.mark.smoke
 def test_complete_purchase(driver):
     login_page = LoginPage(driver)
     inventory_page = InventoryPage(driver)
