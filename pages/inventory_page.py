15725e67cb5f0423f7492048ca237150a38522f6
Nevena90serbiangirl <nevena.suknovic15@gmail.com>
2026-09-26 13:46:16 +0200

    Fix backpack button locator

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,10 +1,14 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class InventoryPage:
     BACKPACK_BUTTON = (
-        By.ID,
-        "add-to-cart-sauce-labs-backpack"
+        By.XPATH,
+        "//div[contains(@class, 'inventory_item')]"
+        "[.//*[normalize-space()='Sauce Labs Backpack']]"
+        "//button[normalize-space()='Add to cart']",
     )
     CART_BADGE = (By.CLASS_NAME, "shopping_cart_badge")
     CART_LINK = (By.CLASS_NAME, "shopping_cart_link")
@@ -13,10 +17,16 @@ class InventoryPage:
         self.driver = driver
 
     def add_backpack_to_cart(self):
-        self.driver.find_element(*self.BACKPACK_BUTTON).click()
+        WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.BACKPACK_BUTTON)
+        ).click()
 
     def get_cart_count(self):
-        return self.driver.find_element(*self.CART_BADGE).text
+        return WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.CART_BADGE)
+        ).text
 
     def open_cart(self):
-        self.driver.find_element(*self.CART_LINK).click()
+        WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.CART_LINK)
+        ).click()
